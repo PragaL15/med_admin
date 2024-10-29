0aa0a30aefe723133611edc758cc9085bb3b94fd
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2024-10-29 09:06:29 +0530

    Record pages are added

--- adminapp/src/components/PatientCard.jsx
+++ adminapp/src/components/PatientCard.jsx
@@ -5,9 +5,9 @@ import { Button } from "primereact/button";
 import MicIcon from "@mui/icons-material/Mic";
 import MicOffIcon from "@mui/icons-material/MicOff";
 import { InputTextarea } from "primereact/inputtextarea";
-
+import Nodata from "../../public/Nodata.png";
 export default function PatientCard() {
-  const [selectedPid, setSelectedPid] = useState(null);
+  const [selectedPid, setSelectedPid] = useState(false);
   const [buttonStatus, setButtonStatus] = useState(false);
   const Pid = [
     { name: "123465" },
@@ -25,8 +25,7 @@ export default function PatientCard() {
   };
 
   return (
-
-    <div className="flex w-full gap-4">
+    <div className="flex w-full gap-4 mt-7">
       {/* <div className="inline w-1/2"> */}
       <Card title="Simple Card" className="w-1/2">
         <div className="card inline justify-content-center">
@@ -40,22 +39,40 @@ export default function PatientCard() {
             valueTemplate={selectedPidTemp}
             className="w-full md:w-14rem bg-white border-2 border-blue-500 rounded-lg shadow-md hover:bg-blue-50 focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
-          <Card className="mt-3">
-            <h1>Pragalya</h1>
+          <Card className="mt-3 flex items-center justify-center">
+            {selectedPid ? (
+              <h1 className="text-center">Pragalya</h1>
+            ) : (
+              <img
+                src="../../public/Nodata.png"
+                alt="No Data"
+                className="w-20 h-24"
+              />
+            )}
           </Card>
         </div>
+        <div className="flex p-1">
         <Button
           onClick={buttonClick}
-          icon={buttonStatus ? <MicIcon /> : <MicOffIcon />}
+          icon={buttonStatus ? <MicIcon /> : <MicOffIcon />  }
           label="Record"
-          className="bg-red-600 text-slate-50 p-2 mt-2"
+          className="bg-red-600 text-slate-50 p-2 mt-2 mr-2"
         />
-        </Card>
-       
-        {/* </div>
-      */}
-      <Card className="w-1/2 flex" title="Converted text">
-        <InputTextarea disabled rows={5} cols={30} value="context will be displayed here" className="md:w-14rem" />
+        {buttonStatus ? <p className="mt-3">Recording started</p> : null}  
+        </div>
+        
+      </Card>
+
+      {/* </div>
+       */}
+      <Card className="w-full md:w-1/2 lg:w-3/5 flex" title="Converted text">
+        <InputTextarea
+          disabled
+          rows={5}
+          cols={30}
+          value="context will be displayed here"
+          className="w-full"
+        />
       </Card>
     </div>
   );
