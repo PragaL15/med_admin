97bad3975add1d02ba48bbb8a8626af76310a54b
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2024-11-04 16:00:24 +0530

    routes are added

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Record from "./pages/Record"
+import Login from "./pages/login"
 import PastRecord from "./pages/PastRecord";
 const RoutesComponent = () => {
   return ( 
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Login/>}/>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path= "/record" element={<Record />} />
         <Route path= "/pastrec" element={<PastRecord/>} />
