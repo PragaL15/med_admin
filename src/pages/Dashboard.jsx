ddceefa9f5545b02c9d01a34885da144419d014d
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2024-11-06 06:29:38 +0530

    Updated dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import Sidebar from '../components/sidebar';
-
+import DBCard from '../components/Dashboard/urgent';
+import Operation from '../components/Dashboard/operation'
+import NoPatients from '../components/Dashboard/CurrentPat'
+import Appoinment from '../components/Dashboard/appoinment'
+import PatientRecoveryGraph from '../components/Dashboard/graph';
+import TopBar from '../components/topNav';
 const Dashboard = () => {
   return (
     <> 
         <div className='h-screen'>
+          <TopBar/>
         <Sidebar /> 
-        <div className='w-full md:pl-60 md:pt-20 md:pr-8 p-3 bg-grey h-full '>
-        </div>
-      </div>
-     
+        <div className='md:flex md:ml-9 md:pl-60 md:pt-10 p-3 bg-grey h-full '>
+          <div className='inline'>
+            <div className='flex'>
+          <DBCard/>
+          <Operation/>
+          <NoPatients/>
+          </div>
+          <PatientRecoveryGraph/>
+          </div>
+          <div className='inline'>
+          <Appoinment/>
+          </div>
+         
+          </div>
+        </div>     
     </>
   );
 }
