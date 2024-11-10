e1ba7c312cab6404a0473e2c693ad4a727088423
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2024-11-10 21:44:47 +0530

    tailwind config is updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,24 @@
 module.exports = {
   content: [
     './index.html',
-    './src/**/*.{js,jsx,ts,tsx}', ],
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {
-        colors: {
-            "side-blue" : "#335c67",
-            "theme-color" :"#f6fff8"
-        },
-        height : {
-          "88" : "22rem"
-        }
+      colors: {
+        "side-blue": "#335c67",
+        "theme-color": "#f6fff8",
+      },
+      height: {
+        "88": "22rem",
+      },
+      fontSize: {
+        "xxs": "10px", // Changed from text to fontSize
+      },
+      fontFamily: {
+        sans: ['Mona Sans', 'sans-serif'], // Use Mona Sans as your default sans font
+      },
     },
   },
   plugins: [],
-}
+};
