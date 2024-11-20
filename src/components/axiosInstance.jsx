ff23b0b352f332d17663a326dfd89b3be5af27e6
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2024-11-20 20:44:30 +0530

    Updated axiosInstance

--- src/components/axiosInstance.jsx
+++ src/components/axiosInstance.jsx
@@ -1,18 +1,14 @@
-// axiosInstance.js
 import axios from 'axios';
 
-// Create an Axios instance
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080',  // Set your base API URL here
+    baseURL: 'http://localhost:8080',  
     headers: {
         'Content-Type': 'application/json',
     },
 });
 
-// Add a request interceptor
 axiosInstance.interceptors.request.use(
     (config) => {
-        // Get the token from localStorage
         const token = localStorage.getItem('authToken');
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
