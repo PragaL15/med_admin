83e3a461fcd9417372bba68c97ae2eb0fc05524a
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2024-12-15 14:45:13 +0530

    Updated past records

--- src/pages/PastRecord.jsx
+++ src/pages/PastRecord.jsx
@@ -12,7 +12,6 @@ const PastRecord = () => {
       <Sidebar />
       <TopBar/>
       <div className="w-full md:pl-60 md:pt-20 md:pr-8 p-3 bg-theme-color h-full">
-        {/* Pass token as a prop to DataRecord if needed */}
         <DataRecord token={token} />
       </div>
     </div>
